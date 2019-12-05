f80971f84f760d90d4ea063d469bbdf9157f3922
coedercyl <2098866016@qq.com>
2019-12-05 20:30:14 +0800

    导航栏及首页

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-  
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+    
+  <main-tab-bar></main-tab-bar>
   
   </div>
 </template>
 
 <script>
-
+import MainTabBar from "components/content/maintabbar/MainTabBar"
 
 export default {
   name: 'app',
   components: {
-    
+    MainTabBar
   }
 }
 </script>
 
 <style>
-  @import "./assets/css/base.css"
+  @import "assets/css/base.css"
 </style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isactive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activestyle"><slot name="item-text"></slot></div>
+</div>
+</template>
+
+<script>
+export default {
+    name:"TabBarItem",
+    props:{
+        path:String,
+        activecolor:{
+            type:String,
+            defult:"red"
+        }
+    },
+    data(){
+        return{
+            
+            
+        }
+        
+    },
+    computed:{
+            isactive(){
+                return this.$route.path.indexOf(this.path)!==-1
+            },
+            activestyle(){
+                return this.isactive ? {color:this.activecolor} : {}
+            }
+        },
+    methods:{
+        itemClick(){
+            this.$router.replace(this.path)
+            
+        }
+    }
+};
+</script>
+
+<style>
+.tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    /* line-height: 49px; */
+    font-size: 14px;
+    }
+.tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    margin-bottom: 2px;
+    vertical-align: middle;
+}
+/* .active{
+    color: red;
+} */
+</style>
